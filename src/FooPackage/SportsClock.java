ea726815e38b4747ea901b01dc1f05ca6fa43067
mesmerizinghermit <m@jellygo.com>
2016-05-21 19:33:09 +0800

    精简ECard模块, 完成SportsClock模块

--- src/FooPackage/SportsClock.java
+++ src/FooPackage/SportsClock.java
@@ -4,7 +4,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
