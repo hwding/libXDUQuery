2fc55f4ed0a75b68b1f2afa80824c8b7384b031a
hwding <m@amastigote.com>
2016-10-09 22:31:55 +0800

    水电查询接口初步完成，单项功能经测可用 thx @TrafalgarZZZ for his hard work! 更新README 规范所有模块内部流程

--- src/com/amastigote/xdu/query/test/Test.java
+++ src/com/amastigote/xdu/query/test/Test.java
@@ -12,37 +12,37 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         try {
-//            PhysicalExperiment physicalExperiment = new PhysicalExperiment();
-//            System.out.println(physicalExperiment.checkIsLogin("15130188016"));
-//            if (physicalExperiment.login("15130188016", "deleted_a")) {
-//                ArrayList<String> stringArrayList_a = physicalExperiment.query();
-//                System.out.println(stringArrayList_a.size());
-//                System.out.println(stringArrayList_a);
-//                System.out.println(physicalExperiment.getID());
-//            }
-//
-//            SportsClock sportsClock = new SportsClock();
-//            if (sportsClock.login("15130188016", "deleted_b")) {
-//                ArrayList<String> stringArrayList_b = sportsClock.query();
-//                System.out.println(stringArrayList_b.size());
-//                System.out.println(stringArrayList_b);
-//                System.out.println(sportsClock.getID());
-//            }
-//
-//            ECard eCard = new ECard();
-//            File file = new File("temp_captcha.jpeg");
-//            eCard.getCaptcha(file);
-//            Scanner scanner = new Scanner(System.in);
-//            String captcha = scanner.nextLine();
-//            if (eCard.login("15130188016", "deleted_c", captcha)) {
-//                ArrayList<String> stringArrayList_c = eCard.query("2016-09-10", "2016-10-06");
-//                System.out.println(stringArrayList_c.size());
-//                System.out.println(stringArrayList_c);
-//            }
-//            file.delete();
+            PhysicalExperiment physicalExperiment = new PhysicalExperiment();
+            System.out.println(physicalExperiment.checkIsLogin("15130188016"));
+            if (physicalExperiment.login("15130188016", "deleted_a")) {
+                ArrayList<String> stringArrayList_a = physicalExperiment.query();
+                System.out.println(stringArrayList_a.size());
+                System.out.println(stringArrayList_a);
+                System.out.println(physicalExperiment.getID());
+            }
+
+            SportsClock sportsClock = new SportsClock();
+            if (sportsClock.login("15130188016", "deleted_b")) {
+                ArrayList<String> stringArrayList_b = sportsClock.query();
+                System.out.println(stringArrayList_b.size());
+                System.out.println(stringArrayList_b);
+                System.out.println(sportsClock.getID());
+            }
+
+            ECard eCard = new ECard();
+            File file = new File("temp_captcha.jpeg");
+            eCard.getCaptcha(file);
+            Scanner scanner = new Scanner(System.in);
+            String captcha = scanner.nextLine();
+            if (eCard.login("15130188016", "deleted_c", captcha)) {
+                ArrayList<String> stringArrayList_c = eCard.query("2016-09-10", "2016-10-06");
+                System.out.println(stringArrayList_c.size());
+                System.out.println(stringArrayList_c);
+            }
+            file.delete();
 
             WaterAndElectricity waterAndElectricity = new WaterAndElectricity();
-            if (waterAndElectricity.login("2011022212","123456")) {
+            if (waterAndElectricity.login("2011022212","deleted_d")) {
                 ArrayList<String> stringArrayList_d = waterAndElectricity.query();
                 System.out.println(stringArrayList_d.size());
                 System.out.println(stringArrayList_d);
